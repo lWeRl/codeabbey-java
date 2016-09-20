1c60663e04f4439e8d094a53495b03aac81948e3
lWeRl <werasdas@gmail.com>
2016-09-20 17:48:34 +0300

    ID 1 Solve!

--- current_problem/src/Current.java
+++ current_problem/src/Current.java
@@ -1,5 +1,16 @@
-/**
- * Created by lWeRl on 20.09.2016.
- */
+import java.util.Scanner;
+
 public class Current {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 0, b = 0;
+        if (sc.hasNextInt()) {
+            a = sc.nextInt();
+        }
+        if (sc.hasNextInt()) {
+            b = sc.nextInt();
+        }
+
+        System.out.println(a + b);
+    }
 }
